783b87ec42df93803738211fd19603109db3d620
ReginaChege <reginachegewairimu@gmail.com>
2023-04-07 16:49:03 +0300

    change

--- array3.js
+++ array3.js
@@ -9,30 +9,45 @@ for(let r=0; r<num.length;r++){
    numbers(num)
 // Write a function that accepts an array of numbers and uses the forEach() method to console.
 // log each number multiplied by 2.
-let numb = [5,7,90,8,6,4,67];
-function arrays(digit){
-   array.forEach(digit => {
-      
-   });
-   console.log(digit*2)
 
-}
-numb.forEach(arrays)
+const number = [2,4,6,8,10,12];
+number.forEach(myFunction)
 
-// // // Write a function that takes in an array of numbers and consoles the first four items multiplied by 8 and the last two added by 5. Console the array with the new values
-function arrays(arr){
-   let empty=[]
-for(let i=0; i<num.length;i++){
-   if(i<4){
-      empty.push(arrays[i]*8);
-   }else if(i>=arrays.length-2){
-      empty.push(arrays[i]+5)
-   }
-   console.log(empty);
+function myFunction(number, index) {
+  number[index] = number * 2;
+  console.log(number*2)
 }
 
+
+// // // Write a function that takes in an array of numbers and consoles the first 
+// four items multiplied by 8 and the last two added by 5. Console the array with the new values
+function arrNu(loop){
+for (i =0; i <4; i++){
+   loop[i] *=8
+ }
+ for(let i=loop.length -2;i<loop.length ; i++) {
+   loop[i] +=5
+ }
+ console.log(loop)
 }
-let arr=[5,7,90,8,6,4,67];
+let loop=[20,30,78,98,90,78]
+arrNu(loop)
+
+// function arrays(arr){
+//    let empty=[];  
+// for(i=0;i<4;i<num.length;i++){
+//    let mun=[5,7,90,8,6,4,67];
+//    let sum=0;
+//    if((i<4)*8){
+//       empty.push(arrays[i]);
+//    }
+//    else{
+//       empty.push(arrays[i]+5)
+//    }
+   
+//    console.log(empty);
+// }
+// }
 
 
 // // Write a function that takes in the following array and use a while loop to 
